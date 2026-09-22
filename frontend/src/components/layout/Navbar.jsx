9afec45f35c8599e198bf7a61b92e43f8d7c5a86
MuscleSenpaiSW6 <139257973+MuscleManiaSW6@users.noreply.github.com>
2026-09-22 23:29:57 +0530

    Navbar...

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -12,17 +12,6 @@ const Navbar = () => {
           className="justify-self-start md:hidden"
           onClick={() => setMenuOpen(true)}
         >
-          {menuOpen && (
-            <div className="fixed inset-0 bg-black/30">
-              <button onClick={() => setMenuOpen(false)}>X</button>
-
-              <nav className="flex flex-col fixed left-0 top-0 bottom-0 items-center gap-8">
-                <Link to="/">Home</Link>
-                <Link to="/products">Shop</Link>
-                <Link to="/account">Account</Link>
-              </nav>
-            </div>
-          )}
           Menu
         </button>
         <Link
@@ -42,6 +31,27 @@ const Navbar = () => {
           Cart
         </Link>
       </div>
+
+      {menuOpen && (
+        <>
+          <div
+            className="fixed inset-0 z-40 bg-black/30 md:hidden"
+            onClick={() => setMenuOpen(false)}
+          >
+            <aside className="fixed left-0 top-0 bottom-0 bg-background md:hidden">
+              <button type="button" onClick={() => setMenuOpen(false)}>
+                X
+              </button>
+            </aside>
+
+            <nav className="flex flex-col fixed left-0 top-0 bottom-0 items-center gap-8">
+              <Link to="/">Home</Link>
+              <Link to="/products">Shop</Link>
+              <Link to="/account">Account</Link>
+            </nav>
+          </div>
+        </>
+      )}
     </header>
   );
 };
