0044844d16f11f78fe39fa05c857e647486c86b8
MuscleSenpaiSW6 <139257973+MuscleManiaSW6@users.noreply.github.com>
2026-09-24 00:09:04 +0530

    Landing Scene

--- frontend/src/components/three/LandingScene.jsx
+++ frontend/src/components/three/LandingScene.jsx
@@ -1,14 +1,27 @@
-import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 const LandingScene = () => {
   return (
     <Canvas>
       <ambientLight intensity={1} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+      <Cube />
     </Canvas>
+  );
+};
+
+const Cube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
   );
 };
 
