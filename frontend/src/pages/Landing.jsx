3746063af98afab347f231eef3b5564ce7d2af1a
MuscleSenpaiSW6 <139257973+MuscleManiaSW6@users.noreply.github.com>
2026-09-24 03:02:07 +0530

    did some changes

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,7 +4,18 @@ const Landing = () => {
   return (
     <main>
       <section className="relative min-h-screen overflow-hidden">
-        <div></div>
+        <div>
+          <p>Modern Essentials</p>
+
+          <h1>
+            LASTDRIP <br /> FROM A DIFFERENT <br /> PERSPECTIVE.
+          </h1>
+
+          <p>Modern clothing for people who move differently.</p>
+
+          <button>ENTER STORE</button>
+        </div>
+
         <LandingScene />
       </section>
     </main>
